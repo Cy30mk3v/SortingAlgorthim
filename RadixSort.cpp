19d3dd646b934049ef5fc341976511b6c7bbea43
Cy30mk3v <40456349+Cy30mk3v@users.noreply.github.com>
2018-11-18 14:44:23 +0700

    Add files via upload

---
+++ RadixSort.cpp
@@ -0,0 +1,23 @@
+#include "Header.h"
+void RadixSort(int *a, int n)
+{
+	int i, b[MAX], m = a[0], exp = 1;
+	for (i = 0; i < n; i++)
+	{
+		if (a[i] > m)
+			m = a[i];
+	}
+	while (m / exp > 0)
+	{
+		int bucket[10] = { 0 };
+		for (i = 0; i < n; i++)
+			bucket[a[i] / exp % 10]++;
+		for (i = 1; i < 10; i++)
+			bucket[i] += bucket[i - 1];
+		for (i = n - 1; i >= 0; i--)
+			b[--bucket[a[i] / exp % 10]] = a[i];
+		for (i = 0; i < n; i++)
+			a[i] = b[i];
+		exp *= 10;
+	}
+}
